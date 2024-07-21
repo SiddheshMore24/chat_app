2cdd97f81b0bf227991ae00b215b915c5cc22898
siddheshmore0024@gmail.com <siddheshmore0024@gmail.com>
2024-07-21 19:27:50 +0530

    commit

---
+++ lib/screens/splash.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ChitChat'),
+
+      ),
+      body: const Center(
+        child: Text('Loading...'),
+      ),
+    );
+  }
+}
